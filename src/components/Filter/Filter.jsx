f04755ac4571cddcea35d4099d8bdd5fcf05e6e2
MoniekNK <monika.nowakowska01@gmail.com>
2023-12-06 19:52:30 +0100

    new

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -10,6 +10,7 @@ const Filter = ({ filter, addFilter }) => (
       className={css.filterInput}
       value={filter}
       placeholder="Enter name"
+      onChange={e => addFilter(e.target.value)}
     />
   </div>
 );
